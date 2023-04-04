3d919d4f98343531cd915e15f70e8fa1a4d067b9
kali <dim.avvakumov@yandex.ru>
2023-04-04 13:51:16 +0300

    -

---
+++ crud_emploeeys/services/get_pk_and_filter_item.py
@@ -0,0 +1,7 @@
+from shop.models.category import Product
+
+
+def get_pk_and_filter_item_by_pk(self):
+    item_pk = self.kwargs.get('pk')
+    item = Product.objects.filter(pk=item_pk).first()
+    return item
